ca70ea50184c0285ec5bd443df9322b587e5cbe8
jopuentes12282985 <166932549+jopuentes12282985@users.noreply.github.com>
2024-12-02 15:13:33 -0500

    j

--- database/tests/reservas.test.js
+++ database/tests/reservas.test.js
@@ -1,15 +1,15 @@
 const assert = require('assert');
-const { validarReserva } = require('../database/reservas'); // Ajusta la ruta según corresponda
+const { validarReserva } = require('../database/reservas');
 
 describe('Pruebas de validación de reservas', function() {
   it('Debería devolver true para una reserva válida', function() {
-    const reservaValida = { clienteNombre: 'Juan', fecha: '2024-01-01', abogado: 'Abogado1', hora: '10:00' };
+    const reservaValida = { clienteNombre: 'Juan', fecha: '2024-01-01', abogados: 'Abogado1', hora: '10:00' };
     const resultado = validarReserva(reservaValida);
     assert.strictEqual(resultado, true);  // Debería ser true
   });
 
   it('Debería devolver false para una reserva inválida', function() {
-    const reservaInvalida = { clienteNombre: '', fecha: '', abogado: '', hora: '' };
+    const reservaInvalida = { clienteNombre: '', fecha: '', abogados: '', hora: '' };
     const resultado = validarReserva(reservaInvalida);
     assert.strictEqual(resultado, false);  // Debería ser false
   });
